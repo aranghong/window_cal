45626d58dcc4fc6af36ab206e6bf9b81ee1a9f77
HyunBaeL <seehb126@naver.com>
2025-05-03 15:44:24 +0900

    Mul,Sub,Div 클래스 추가

---
+++ window_cal/Div.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace window_cal
+{
+    internal class Div
+    {
+        public double div(string word)
+        {
+            int index = word.IndexOf("%");
+            string a = word.Substring(0, index).Trim();
+            double s = double.Parse(a);
+            string b = word.Substring(index + 2).Trim();
+            double ss = double.Parse(b);
+            return (s / ss) + (s % ss);
+        }
+    }
+}
---
+++ window_cal/Mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace window_cal
+{
+    internal class Mul
+    {
+        public double mul(string word)
+        {
+            int index = word.IndexOf("X");
+            string a = word.Substring(0, index).Trim();
+            double s = double.Parse(a);
+            string b = word.Substring(index + 2).Trim();
+            double ss = double.Parse(b);
+            return s * ss;
+        }
+    }
+}
